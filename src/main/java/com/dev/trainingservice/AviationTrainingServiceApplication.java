3d372ed424b751dad5f523c1f7c1f0dcbd0b627c
Kevin Llanos <kllano2@wgu.edu>
2026-01-09 19:14:28 -0600

    spring initializer setup

---
+++ src/main/java/com/dev/trainingservice/AviationTrainingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dev.trainingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AviationTrainingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AviationTrainingServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dev/trainingservice/AviationTrainingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.trainingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AviationTrainingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
